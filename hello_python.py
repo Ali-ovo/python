ef03e984d4f33cb9f01e7d0193b0b8abebfb9221
Ali <ali_ovo@qq.com>
2025-05-09 11:55:38 +0800

    feat: init python

---
+++ hello_python.py
@@ -0,0 +1,73 @@
+print("Hello, World!")
+
+
+num1 = 10
+num2 = 20
+
+sum = num1 + num2
+
+print(sum)
+
+
+name = "John"
+age = 20
+
+print(name, age)
+
+
+# int
+int1 = 10
+
+# float
+float1 = 10.5
+
+# string
+string1 = "Hello"
+
+# boolean
+boolean1 = True
+
+print(int1)
+print(float1)
+print(string1)
+print(boolean1)
+
+
+# list
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+
+# tuple
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1)
+
+# dictionary
+dict1 = {"name": "John", "age": 20}
+print(dict1)
+
+# set
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+
+
+print("今年 %d 歲" % age)
+print("name: %s, age: %d" % (name, age))
+
+height = 1.7
+print("my name is %s, my age is %d, my height is %f" % (name, age, height))
+
+print("小敏", "小帅", "小明", sep="-")
+print("这是第一行", end=",")
+print("这是第二行", end=",")
+print("这是第三行")
+
+# 輸入
+# name = input('請輸入你的名字: ')
+# age = input('請輸入你的年齡: ')
+# print(name, age)
+
+str1 = '11'
+int1 = int(str1)
+print(int1)
+
+
